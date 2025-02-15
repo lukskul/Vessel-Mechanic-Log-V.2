ce1930d0b8789af0d1f7a6abb94824dd6430fd38
lukSkul <lukskul.nft@gmail.com>
2025-02-14 19:00:15 -0800

    working out the bugs

--- App/js/index.js
+++ App/js/index.js
@@ -5,13 +5,13 @@ import '../css/menu.css';
 import '../css/button.css'; 
 import '../css/animation.css'; 
 import '../css/alert.css'; 
-import '../css/tasks.css';  
+import '../css/tasks.css'; 
 
 // Javascript
 import './language.js';     
 import './alert.js'; 
 import './vessel.js'; 
-import './taskDisplay.js';
+
 
 
 
