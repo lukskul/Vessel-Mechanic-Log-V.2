60d78664396b86d121e9c2da2050bc5809d930da
lukSkul <lukskul.nft@gmail.com>
2025-03-15 10:17:29 -0700

    adding prop drop down icon

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch-all route for index.html (must be last)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
