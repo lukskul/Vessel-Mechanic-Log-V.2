182846dc4c539aaeb5c36b8553c7c51ce8e89718
lukSkul <lukskul.nft@gmail.com>
2025-11-15 11:55:27 -0800

    naming conflict

--- App/js/menu.js
+++ App/js/menu.js
@@ -184,7 +184,7 @@ document.getElementById("search-ship-button").addEventListener("click", () => {
     const newTab = window.open("", "_blank");
 
     // Fetch vessel info from info.json
-    fetch(`https://lukskul.github.io/Vessel-Mechanic-Log-V.2/DataFiles/${selectedBoat}/info.json`)
+    fetch(`https://lukskul.github.io/Vessel-Mechanic-Log/DataFiles/${selectedBoat}/info.json`)
         .then(response => response.json())
         .then(data => {
             const vessel = data.shipDetails;
@@ -260,7 +260,7 @@ const languageIcon = languageSelectButton.querySelector("img");
 // Function to update the button icon based on the current language
 function updateLanguageIcon() {
     const currentLang = localStorage.getItem("language") || "en";
-    languageIcon.src = currentLang === "en" ? "//lukskul.github.io/Vessel-Mechanic-Log-V.2/App/icons/usa.svg" : "//lukskul.github.io/Vessel-Mechanic-Log-V.2/App/icons/mexico.svg";
+    languageIcon.src = currentLang === "en" ? "//lukskul.github.io/Vessel-Mechanic-Log/App/icons/usa.svg" : "//lukskul.github.io/Vessel-Mechanic-Log-V.2/App/icons/mexico.svg";
 }
 
 // Run the function on page load to ensure the correct icon is displayed
