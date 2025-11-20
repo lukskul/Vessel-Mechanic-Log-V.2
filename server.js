dd22e660619fbf5016be76d1d39e934d0c6f60f2
lukSkul <lukskul.nft@gmail.com>
2025-11-20 06:17:46 -0800

    update

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from dist
+// Serve static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Serve JSON files from DataFiles
-app.use('/DataFiles', express.static(path.join(__dirname, 'DataFiles')));
-
-// Catch-all route (must be last)
+// Catch-all route for index.html (must be last)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'app', 'index.html'));
 });
@@ -19,4 +16,5 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
 
