be4a2a9653ba2ca034b4a7ad8fb9eebfa25da4df
lukSkul <lukskul.nft@gmail.com>
2025-11-20 19:36:21 -0800

    update styles

--- App/js/menu.js
+++ App/js/menu.js
@@ -142,9 +142,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Caption
         const caption = document.createElement("caption");
-        if (type === 'socket') caption.textContent = "Standard Socket to Metric Socket";
-        else if (type === 'tap') caption.textContent = "Tap and Drill Chart<br><br>(New Hole)";
-        else if (type === 'dimple') caption.textContent = "Set Screw Dimple chart<br><br>";
+        if (type === 'socket') caption.textContent =      "Standard Socket to Metric Socket";
+        else if (type === 'tap') caption.textContent =    "Tap and Drill Chart   (New Hole)";
+        else if (type === 'dimple') caption.textContent = "Set Screw Dimple chart          ";
         table.appendChild(caption);
 
         // Headers
